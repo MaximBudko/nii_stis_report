6144cf3535cfbef2ad7fa282c94052ec675410e2
Maxim Budko <maksimbudko16@gmail.com>
2025-01-17 06:45:24 +0000

    rework report generation

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ Redmine::Plugin.register :monthly_report do
   name 'Monthly Report plugin'
   author 'Maxim Budko'
   description 'Генерация ежемесячного отчета НИИ СТИС'
-  version '1.0.0'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  version '1.0.3'
+  url 'https://github.com/MaximBudko/nii_stis_report'
+  author_url 'https://github.com/MaximBudko'
 
   
   menu :top_menu, :report_generator, {controller: 'report', action: 'index'}, caption: 'Генератор отчетов' , if: Proc.new { User.current.admin? }
