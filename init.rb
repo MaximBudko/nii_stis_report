42e52f7a4378efec5f581dc8bbc043023aa9b4ad
Maxim Budko <maksimbudko16@gmail.com>
2025-01-31 05:14:15 +0000

    add roles and permission

--- init.rb
+++ init.rb
@@ -7,5 +7,13 @@ Redmine::Plugin.register :monthly_report do
   author_url 'https://github.com/MaximBudko'
 
   
-  menu :top_menu, :report_generator, {controller: 'report', action: 'index'}, caption: 'Генератор отчетов' , if: Proc.new { User.current.admin? }
+  menu :top_menu, :report_generator, {controller: 'report', action: 'index'}, caption: 'Генератор отчетов' , if: Proc.new { User.current.allowed_to_see_plugin? }
+end
+
+
+User.class_eval do
+  def allowed_to_see_plugin?
+    custom_field_value = self.custom_field_value(CustomField.find_by_name('Plugin_visibility').id)
+    custom_field_value == 'Да'
+  end
 end
